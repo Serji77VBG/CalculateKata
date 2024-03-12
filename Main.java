e8d50328f941b07c18d7aef6b37de6d171e757e3
Serji77VBG <159805477+Serji77VBG@users.noreply.github.com>
2024-03-12 23:00:27 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 import static java.lang.Integer.parseInt;
+import static java.lang.Integer.valueOf;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,10 +11,13 @@ public class Main {
         String answer = calc(input);
         System.out.println("Output:");
         System.out.print(answer);
+
     }
 
     public static String calc(String input) {
         int result = 0;
+
+
         String error = "Throws Exception";
         String[] inputSplit = input.split(" ");
         if (inputSplit.length != 3) {
@@ -24,30 +28,54 @@ public class Main {
         try {
             a = parseInt(inputSplit[0]);
             b = parseInt(inputSplit[2]);
-        }catch(NumberFormatException e){
-            return error;
-        }
-
-        String sign = inputSplit[1];
-        if ((a < 1) || (a > 10) || (b < 1) || (b > 10)) {
-            return error;
-        }
-        switch (sign) {
-            case "+" -> result = a + b;
-            case "-" -> result = a - b;
-            case "*" -> result = a * b;
-            case "/" -> result = a / b;
-            default -> {
+        } catch (NumberFormatException e) {
+            try {
+                a = romaToArab(inputSplit[0]);
+                b = romaToArab(inputSplit[2]);
+            } catch (NumberFormatException ex) {
                 return error;
+            }
+
+            String sign = inputSplit[1];
+            if ((a < 1) || (a > 10) || (b < 1) || (b > 10)) {
+                return error;
+            }
+            switch (sign) {
+                case "+" -> result = a + b;
+                case "-" -> result = a - b;
+                case "*" -> result = a * b;
+                case "/" -> result = a / b;
+                default -> {
+                    return error;
+                }
             }
         }
         String output = Integer.toString(result);
-
         return output;
+    }
 
 
+     static int romaToArab(String romaNumber) {
+        int result = 0;
+        int index = 0;
+        int[] arab = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        String[] roma = {"X", "IX", "VIII", "VII", "VI", "V", "IV",  "III", "II", "I"};
+        for (int i = 0; i < roma.length; i++) {
+            if (roma[i].equals(romaNumber)) {
+                index = i;
+
+            }
+
+        }
+        result = arab[index];
+
+        return result;
     }
 }
+
+
+
+
 
 
 
